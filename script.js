b654b7f0b38d43632fb0be26883a5f32b5e02e62
Noga Gercsak <135066149+nogagercsak@users.noreply.github.com>
2023-10-01 15:14:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-// JavaScript for scrolling left and right
-document.querySelector('.scroll-left-button').addEventListener('click', function () {
-    document.querySelector('.project-gallery').scrollLeft -= 300; // Adjust as needed
+// Smooth scrolling when clicking on navigation links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1).toLowerCase(); // Convert to lowercase
+        const targetSection = document.getElementById(targetId);
+
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop,
+                behavior: 'smooth'
+            });
+        }
+    });
 });
 
-document.querySelector('.scroll-right-button').addEventListener('click', function () {
-    document.querySelector('.project-gallery').scrollLeft += 300; // Adjust as needed
-});
+
