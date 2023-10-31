5832c503755012cddee8cb7b0be8cef777f977cf
Noga Gercsak <135066149+nogagercsak@users.noreply.github.com>
2023-10-30 20:23:29 -0400

    changes

--- script.js
+++ script.js
@@ -15,5 +15,23 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
+//typing effect for title
+const nameElement = document.getElementById("nameTitle");
+const nameText = nameElement.textContent;
+nameElement.textContent = ""; // Clear the initial text
 
 
+let index = 0;
+
+
+function typeNextLetter() {
+if (index < nameText.length) {
+nameElement.textContent += nameText.charAt(index);
+index++;
+setTimeout(typeNextLetter, 100); // Adjust the typing speed (in milliseconds) here
+}
+}
+
+
+// Start the typing effect when the page loads
+typeNextLetter();
